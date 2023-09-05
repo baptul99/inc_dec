1a5a9459206a7cd7bab719408293e36a8de121f9
baptul99 <baptulchangdar@gmail.com>
2023-09-05 20:28:24 +0530

    baptul

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import UseState from './Hooks/useState';
+// import UseEffect from './Hooks/useEffect';
+import UseReducer from './Hooks/useReducer';
+
+
+const App = () => {
+  return <UseReducer/>
+ 
+}
+
+export default App;
